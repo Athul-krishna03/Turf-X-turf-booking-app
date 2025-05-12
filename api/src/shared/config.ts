2fa1a5a6ad6a5995ffbc90aaafb8d4bbd1ff80d2
ATHUL0527 <www.athulappu0527@gmail.com>
2025-05-12 21:58:01 +0530

    shared slot db design and ui

--- api/src/shared/config.ts
+++ api/src/shared/config.ts
@@ -5,22 +5,22 @@ dotenv.config({path:path.resolve(__dirname,"../../.env")})
 export const config = {
     cors:{
         ALLOWED_ORGIN:
-           process.env.CORS_ALLOWED_ORIGIN || "http://localhost:5173"
+           process.env.CORS_ALLOWED_ORIGIN 
     },
     server:{
-        PORT:process.env.PORT || 5000,
-        NODE_ENV:process.env.NODE_ENV || "development"
+        PORT:process.env.PORT ,
+        NODE_ENV:process.env.NODE_ENV 
     },
     database:{
         URI:process.env.MONGODB_URI!
     },
     jwt:{
-        ACCESS_SECRET_KEY: process.env.JWT_ACCESS_KEY || "access-secret-key",
-		REFRESH_SECRET_KEY: process.env.JWT_REFRESH_KEY || "refresh-secret-key",
-		ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN || "15m",
-        REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN || "7d",
-        RESET_SECRET_KEY:process.env.JWT_RESET_KEY || "reset-secret-key",
-        RESET_EXPIRES_IN:process.env.JWT_RESET_EXPIRES_IN || "5m"
+        ACCESS_SECRET_KEY: process.env.JWT_ACCESS_KEY ,
+		REFRESH_SECRET_KEY: process.env.JWT_REFRESH_KEY ,
+		ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN ,
+        REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN ,
+        RESET_SECRET_KEY:process.env.JWT_RESET_KEY ,
+        RESET_EXPIRES_IN:process.env.JWT_RESET_EXPIRES_IN 
     },
     nodemailer:{
         EMAIL_USER:process.env.NODEMAILER_EMAIL,
@@ -29,7 +29,7 @@ export const config = {
     redis:{
         redisURL:process.env.REDIS_URL
     },
-    loggerStatus: process.env.LOGGER_STATUS || "dev",
-    bcryptSaltRounds: parseInt(process.env.BCRYPT_SALT_ROUNDS || "10", 10),
+    loggerStatus: process.env.LOGGER_STATUS ,
+    bcryptSaltRounds: parseInt(process.env.BCRYPT_SALT_ROUNDS || "", 10),
     stripe:process.env.STRIPE_SECRET_KEY || ""
 }
